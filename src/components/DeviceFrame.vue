5f6a4a676898a3dac3883cede3a4d6f11d1e74f0
cip cir <cipcir@gmail.com>
2019-10-14 20:20:11 +0300

    de acasa

--- src/components/DeviceFrame.vue
+++ src/components/DeviceFrame.vue
@@ -1,53 +1,74 @@
 <template>
-  <div id="iframeContainer" @click.self="closeiFrame()">
-    <div id="FrameDimControls" class="Frm_row">
+  <div id="iframeContainer">
+    <center @click.self="closeiFrame()">
+      <div id="FrameDimControls" class="Frm_row">
+        <div
+          class="deviceItmFrame Frm_col"
+          v-for="(device,index) in deviceBtns"
+          :key="index"
+          :class="{'selDevice':device.lbl==SelDeviceBtn.lbl}"
+          @click="SetDeviceLayout(device)"
+        >
+          <i :class="[device.icon,{'landSk':device.landSc},device.color]"></i>
+        </div>
+        <div class="ctrlZone Frm_col Frm_row">
+          <span class="Frm_col">Width:</span>
+          <input
+            type="range"
+            class="FrameSlider Frm_col"
+            v-model="iFrame_Width"
+            min="360"
+            max="1024"
+            @change="UpdateDevice()"
+          />
+          <span class="Frm_col">{{iFrame_Width}}</span>
+          <span class="dimLbl Frm_col">px</span>
+        </div>
+        <div class="ctrlZone Frm_col Frm_row">
+          <span>Height:</span>
+          <input
+            type="range"
+            class="FrameSlider"
+            v-model="iFrame_Height"
+            min="360"
+            max="1024"
+            @change="UpdateDevice()"
+          />
+          <span class="Frm_col">{{iFrame_Height}}</span>
+          <span class="dimLbl Frm_col">px</span>
+        </div>
+        <div class="ctrlZone Frm_col Frm_row">
+          <span>Zoom:</span>
+          <input
+            type="range"
+            class="FrameSlider"
+            min="0.5"
+            max="1"
+            step="0.1"
+            v-model="iFrame_scale"
+          />
+          <span class="Frm_col">{{iFrame_scale}}</span>
+        </div>
+      </div>
+
+      <div id="TxtCont" class="Frm_row">
+        <i class="fas fa-info-circle" style="margin-right:5px"></i>This is a simulation of a device screen, actual layout might vary on a real device.
+      </div>
+
       <div
-        class="deviceItmFrame Frm_col"
-        v-for="(device,index) in deviceBtns"
-        :key="index"
-        :class="{'selDevice':device.lbl==SelDeviceBtn.lbl}"
-        @click="SetDeviceLayout(device)"
+        class="smartphone"
+        id="iFrameDiv"
+        :class="SelDeviceBtn.landSc?'lansc':'portrt'"
+        :style="'transform: scale('+iFrame_scale+');transform-origin:50% 0;'"
       >
-        <i :class="[device.icon,{'landSk':device.landSc},device.color]"></i>
-      </div>
-      <div class="ctrlZone Frm_col Frm_row">
-        <span class="Frm_col">Width:</span>
-        <input
-          type="range"
-          class="FrameSlider Frm_col"
-          v-model="iFrame_Width"
-          min="400"
-          max="1024"
-          @change="UpdateDevice()"
+        <iframe
+          :src="iFrameSrc"
+          frameborder="0"
+          id="ESQFrame"
+          :style="'overflow:hidden;height:' + iFrame_Height +'px;width:' + iFrame_Width+'px;'"
         />
-        <span class="Frm_col">{{iFrame_Width}}</span>
-        <span class="dimLbl Frm_col">px</span>
       </div>
-      <div class="ctrlZone Frm_col Frm_row">
-        <span>Height:</span>
-        <input
-          type="range"
-          class="FrameSlider"
-          v-model="iFrame_Height"
-          min="400"
-          max="1024"
-          @change="UpdateDevice()"
-        />
-        <span class="Frm_col">{{iFrame_Height}}</span>
-        <span class="dimLbl Frm_col">px</span>
-      </div>
-    </div>
-    <div id="TxtCont" class="Frm_row">
-      <i class="fas fa-info-circle" style="margin-right:5px"></i>This is a simulation of a device screen, actual layout might vary on a real device.
-    </div>
-    <div class="smartphone Frm_row" :class="SelDeviceBtn.landSc?'lansc':'portrt'">
-      <iframe
-        :src="iFrameSrc"
-        frameborder="0"
-        id="ESQFrame"
-        :style="'overflow:hidden;height:' + iFrame_Height +'px;width:' + iFrame_Width+'px;'"
-      />
-    </div>
+    </center>
     <div id="CloseFrame" @click="closeiFrame()">
       <i class="far fa-window-close"></i>
     </div>
@@ -63,6 +84,7 @@ export default {
         "https://media.ipsosinteractive.com/applications/SurveyShortcuts/FishTemplate.html",
       iFrame_Width: 411,
       iFrame_Height: 731,
+      iFrame_scale: "1",
       SelDeviceBtn: { lbl: "", landSc: false }
     };
   },
@@ -93,7 +115,8 @@ export default {
     },
     UpdateDevice() {
       this.SelDeviceBtn.lbl = "";
-      this.SelDeviceBtn.landSc = this.iFrame_Width > this.iFrame_Height;
+      this.SelDeviceBtn.landSc =
+        parseInt(this.iFrame_Width) > parseInt(this.iFrame_Height);
     }
   }
 };
@@ -102,7 +125,7 @@ export default {
 .deviceItmFrame {
   width: 40px;
 }
-.Frm_row {
+/* .Frm_row {
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 20px;
@@ -112,24 +135,22 @@ export default {
   display: table;
   clear: both;
 }
-/* .Frm_row .Frm_col {
-  margin-left: -0.75rem;
-  margin-right: -0.75rem;
-} */
 .Frm_row .Frm_col {
   float: left;
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
   padding: 0 0.75rem;
   min-height: 1px;
-}
+} */
 
 #TxtCont {
+  /* margin: 0 auto; */
+  width: 70%;
   background: white;
   color: black;
   padding: 5px;
   border-radius: 3px;
-  margin-bottom: 5px;
+  margin: 5px;
 }
 #iframeContainer {
   /* display: flex; */
@@ -147,20 +168,120 @@ export default {
   overflow: auto;
 }
 
-.FrameSlider {
-  /* -webkit-appearance: none; */
-  /* width: 170px !important; */
-  /* height: 15px; */
-  /* border-radius: 5px; */
-  /* background: #d3d3d3;
+input[type="range"] {
+  /* fix for FF unable to apply focus style bug  */
+  border: 1px solid white;
+
+  /*required for proper track sizing in FF*/
+  width: 300px;
+}
+
+input[type="range"]::-moz-range-track {
+  width: 300px;
+  height: 5px;
+  background: #ddd;
+  border: none;
+  border-radius: 3px;
+}
+
+input[type="range"]::-moz-range-thumb {
+  border: none;
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  background: goldenrod;
+}
+
+/*hide the outline behind the border*/
+input[type="range"]:-moz-focusring {
+  outline: 1px solid white;
+  outline-offset: -1px;
+}
+
+input[type="range"]:focus::-moz-range-track {
+  background: #ccc;
+}
+
+input[type="range"] {
+  -webkit-appearance: none;
+}
+
+input[type="range"]::-webkit-slider-runnable-track {
+  width: 300px;
+  height: 5px;
+  background: #ddd;
+  border: none;
+  border-radius: 3px;
+}
+
+input[type="range"]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  border: none;
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  background: goldenrod;
+  margin-top: -4px;
+}
+
+input[type="range"]:focus {
+  outline: none;
+}
+
+input[type="range"]:focus::-webkit-slider-runnable-track {
+  background: #ccc;
+}
+
+input[type="range"]::-ms-track {
+  width: 300px;
+  height: 5px;
+
+  /*remove bg colour from the track, we'll use ms-fill-lower and ms-fill-upper instead */
+  background: transparent;
+
+  /*leave room for the larger thumb to overflow with a transparent border */
+  border-color: transparent;
+  border-width: 6px 0;
+
+  /*remove default tick marks*/
+  color: transparent;
+}
+input[type="range"]::-ms-fill-lower {
+  background: #777;
+  border-radius: 10px;
+}
+input[type="range"]::-ms-fill-upper {
+  background: #ddd;
+  border-radius: 10px;
+}
+input[type="range"]::-ms-thumb {
+  border: none;
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  background: goldenrod;
+}
+input[type="range"]:focus::-ms-fill-lower {
+  background: #888;
+}
+input[type="range"]:focus::-ms-fill-upper {
+  background: #ccc;
+}
+
+/* .FrameSlider {
+  -webkit-appearance: none;
+  width: 170px !important;
+  height: 15px;
+  border-radius: 5px;
+  background: #d3d3d3;
   outline: none;
   opacity: 0.7;
-  margin: 0 5px; */
+  margin: 0 5px;
   -webkit-transition: 0.2s;
   transition: opacity 0.2s;
 }
 
-/* .FrameSlider::-webkit-slider-thumb {
+.FrameSlider::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
   width: 25px;
@@ -189,6 +310,7 @@ export default {
   color: #faea9c;
 }
 #FrameDimControls {
+  text-align: center;
   min-width: 637px;
   background: ghostwhite;
   width: 70%;
@@ -197,21 +319,26 @@ export default {
   /* display: flex;
   align-items: center;
   justify-content: center; */
+  margin: 0 auto;
   font-weight: bold;
   box-shadow: 0 8px 17px 2px rgba(0, 0, 0, 0.14),
     0 3px 14px 2px rgba(0, 0, 0, 0.12), 0 5px 5px -3px rgba(0, 0, 0, 0.2);
 }
-#FrameDimControls > input {
+.ctrlZone > input {
+  display: inline;
+  width: 100px;
   margin: 0 5px;
 }
 .ctrlZone {
   margin: 0 5px;
-  width: 30%;
+  /* width: 30%; */
+  display: inline;
   /* display: flex;
   justify-content: center; */
 }
 
 .deviceItmFrame {
+  display: inline;
   background: lightblue;
   padding: 5px;
   cursor: pointer;
@@ -242,6 +369,10 @@ export default {
   background: white;
 }
 /* The device with borders */
+#iFrameDiv {
+  margin: 0 auto;
+  display: inline-block;
+}
 .smartphone {
   position: relative;
   margin: auto;
